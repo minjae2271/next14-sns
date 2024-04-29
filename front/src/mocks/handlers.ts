31cb2a88fce76c6fb69eaf404e22f8c66db6f8ed
minjae <minjae2271@naver.com>
2024-04-29 22:50:57 +0900

    msw, env, signup

--- front/src/mocks/handlers.ts
+++ front/src/mocks/handlers.ts
@@ -1,19 +1,22 @@
 import { http, HttpResponse } from 'msw';
 
 export const handlers = [
+    http.post('/api/users', () => {
+      console.log('회원가입');
+      return HttpResponse.text(JSON.stringify('users_exists'), {
+        status: 403,
+      })
+      // return new HttpResponse(null, {
+      //   headers: {
+      //     'Set-Cookie': 'connect.sid=;HttpOnly;Path=/;Max-Age=0'
+      //   }
+      // })
+    }),
     http.post('/api/login', () => {
         console.log('로그인');
         return HttpResponse.json({id: 'elonmusk', nickname: 'Elon Musk', image: '/yRsRRjGO.jpg'}, {
           headers: {
             'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
-          }
-        })
-      }),
-      http.post('/api/logout', () => {
-        console.log('로그아웃');
-        return new HttpResponse(null, {
-          headers: {
-            'Set-Cookie': 'connect.sid=;HttpOnly;Path=/;Max-Age=0'
           }
         })
       }),
