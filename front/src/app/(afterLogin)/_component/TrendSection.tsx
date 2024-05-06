21272d85d6b73652509b1a78c84f17f2a7fea59e
minjae <minjae2271@naver.com>
2024-05-06 13:38:43 +0900

    react-query

--- front/src/app/(afterLogin)/_component/TrendSection.tsx
+++ front/src/app/(afterLogin)/_component/TrendSection.tsx
@@ -3,20 +3,33 @@
 import styles from './trendSection.module.css';
 import Trend from './Trend';
 import { usePathname } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
 export default function TrendSection() {
     const pathname = usePathname(); // /부터 ?까지
+    const { data } = useSession();
+
     if(pathname === '/explore') return null;
+
+    if ( data?.user) {
+        return (
+            <div className={styles.trendBg}>
+                <div className={styles.trend}>
+                    <h3>나를 위한 트렌드</h3>
+                    <Trend />
+                    <Trend />
+                    <Trend />
+                    <Trend />
+                    <Trend />
+                    <Trend />
+                </div>
+            </div>
+        )
+    }
     return (
         <div className={styles.trendBg}>
             <div className={styles.trend}>
-                <h3>나를 위한 트렌드</h3>
-                <Trend />
-                <Trend />
-                <Trend />
-                <Trend />
-                <Trend />
-                <Trend />
+                <h3>트렌드를 가져 올 수 없습니다.</h3>
             </div>
         </div>
     )
