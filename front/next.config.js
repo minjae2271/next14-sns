db45dae69b9d39ab999927c0846704e95c8cd4a3
minjae <minjae2271@naver.com>
2024-05-22 22:13:46 +0900

    preview images

---
+++ front/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/upload/:slug',
+          destination: `http://localhost:9090/upload/:slug`, // Matched parameters can be used in the destination
+        },
+      ]
+    },
+  }
+  
+  module.exports = nextConfig
