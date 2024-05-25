fa0d2d0bcc3036ec33fc7518ef111e9e8950b4ce
minjae <minjae2271@naver.com>
2024-05-25 23:46:20 +0900

    hashtag search

--- front/src/app/(afterLogin)/_component/Trend.tsx
+++ front/src/app/(afterLogin)/_component/Trend.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function Trend({ trend } : Props) {
     return (
-        <Link href={`/search?q=${trend.title}`} className={styles.container}>
+        <Link href={`/search?q=${encodeURIComponent(trend.title)}`} className={styles.container}>
             <div className={styles.count}>실시간 트렌드</div>
             <div className={styles.title}>{trend.title}</div>
             <div className={styles.count}>{trend.count.toLocaleString()}</div>
--- front/src/app/(afterLogin)/search/_lib/getSearchResult.ts
+++ front/src/app/(afterLogin)/search/_lib/getSearchResult.ts
@@ -3,12 +3,14 @@ import { QueryFunction } from "@tanstack/react-query";
 
 export const getSearchResult: QueryFunction<Post[], [_1: string, _2: string, searchParams: { q: string, f?: string, pf?: string }]>
     =  async ({queryKey}) => {
-    const [_1, _2, searchParams] = queryKey
-    const res = await fetch(`http://localhost:9090/api/search/${searchParams.q}?${searchParams.toString()}`, {
+    const [_1, _2, searchParams] = queryKey;
+    const urlSearchParams = new URLSearchParams(searchParams);
+    const res = await fetch(`http://localhost:9090/api/posts?${urlSearchParams.toString()}`, {
        next: {
            tags: ["posts", "search", searchParams.q], //next caching은 객체 사용 불가!
        },
        cache: "no-store",
+       credentials: 'include'
     });
     if (!res.ok) {
        throw new Error("Failed to fetch search data");
